dffa237dd118701f92c8c89564f18e7fecf224bf
AmanDeep9925 <amandeep58257@gmail.com>
2020-06-01 16:18:11 +0530

    Added Posts route

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,3 +1,7 @@
+module.exports.user = (req,res) =>{
+    res.end(`<h1>Welcom to your account </h1>`);
+}
+
 module.exports.profile = (req,res) =>{
     res.end(`<h1>Profile Router</h1>`);
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,13 @@ const router = express.Router();
 
 const userController = require('../controllers/users_controllers');
 
+
+// users page
+
+router.get('/',userController.user)
+
 router.get('/profile',userController.profile);
+
+router.get('/posts',require('./posts'));
 
 module.exports = router;
