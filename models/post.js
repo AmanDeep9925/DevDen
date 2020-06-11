169d043c64dfe1d2cf20b90890fc49b10907280e
AmanDeep9925 <amandeep58257@gmail.com>
2020-06-11 21:58:37 +0530

    Created Comment Schema

--- models/post.js
+++ models/post.js
@@ -8,7 +8,14 @@ const postSchema = new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    // include the arrays of ids of comments
+    comments : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'comment'
+        }
+    ]
 },{
     timestamps:true
 });
