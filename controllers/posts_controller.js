03bff04077b114e702b6ff138b3f5775acc039fb
AmanDeep9925 <amandeep58257@gmail.com>
2020-06-27 17:20:10 +0530

    Ajax Request

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -16,10 +16,19 @@ module.exports.create = async (req, res) => {
     // })
 
     try {
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data : {
+                    post : post
+                },
+                message : "Post Created"
+            })
+        }
         req.flash('success',"Post a new story :)");
         return res.redirect('back');
     } catch (error) {
